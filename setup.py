bc799d5f940f02bd86267fbceaa1953201749d19
oxpeter <oxpeter+git@gmail.com>
2014-08-27 22:55:00 -0400

    created setup.py for easy install!
    Having better arranged the modules according to python best-
    practice, the setup.py script should also allow easier
    incorporation into other computers. Setup includes addition
    of the data file, to which the new pathways.cfg will be
    located on creation.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Genomepy',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Peter Oxley',
+      author_email='oxpeter+git@gmail.com',
+      url='https://github.com/oxpeter/genomepy',
+      packages=['genomepy'],
+      package_dir={'genomepy': 'genomepy'},
+      package_data={'genomepy': ['data/*.cfg']},
+      requires=['argparse','scipy','numpy','matplotlib','Bio','qvalue', 'progressbar',
+                'operator', 'pylab', 'mpl_toolkits', 'statsmodels', 'pysam'
+                ]
+     )
