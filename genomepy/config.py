67bba61a585fd9b8586cf704842b4b18d0e645ab
oxpeter <oxpeter+git@gmail.com>
2015-01-06 12:13:46 -0500

    transferred all administrative/file-manipulation fns to config.py
    verbalise, pickle functions, make_a_list were all moved from
    genematch to config, allowing this module to serve not just as a
    link to the file containing the data file locations, but to allow
    more intuitive locating of generic functions.

--- genomepy/config.py
+++ genomepy/config.py
@@ -8,12 +8,42 @@ Later improvements should include storing the object as a pickled object for fas
 access.
 """
 
-import os
+import os, sys
 import re
+import time
 from subprocess import Popen, PIPE
 from operator import itemgetter
 from pkg_resources import resource_filename, resource_exists
 #import pkgutil
+
+def verbalise(arg1, *args):
+    # define escape code: '\x1b[31m  %s  \x1b[0m'
+    colordict = {'R':'\x1b[31m', 'G':'\x1b[32m',
+         'Y':'\x1b[33m' ,'B':'\x1b[34m', 'M':'\x1b[35m' , 'C':'\x1b[36m' }
+    if arg1 in colordict:
+        argstring = " ".join([str(arg) for arg in args])
+        if sys.stdout.isatty():
+            color_code = colordict[arg1]
+            end_color = '\x1b[0m'
+        else:
+            color_code = ""
+            end_color = ""
+    else:
+        argstring = " ".join([arg1] + [arg for arg in args])
+        color_code = ""
+        end_color = ""
+
+    print "%s%s%s" % (color_code, argstring, end_color)
+
+def check_verbose(v=True):
+    "allow optional printing with color conversion capability!"
+    global verbalise
+    if v:
+        verbalise = verbalise
+    else:
+        verbalise = lambda *a: None
+
+    return verbalise
 
 def read_pathways(config_file):
     pathway_dict = {}
@@ -130,6 +160,143 @@ def import_paths():
     else:
         pathway_dict = construct_pathways(config_path)
     return pathway_dict
+
+
+##### FILE MANIPULATION #####
+def pickle_jar(obj, fname):
+    pklfile = ".".join([fname, 'pkl'])
+    apicklefile = open(pklfile, 'wb')
+    cPickle.dump(obj, apicklefile, -1)
+    apicklefile.close()
+
+def open_pickle_jar(fname):
+    pklfile = ".".join([fname, 'pkl'])
+    apicklefile = open(pklfile, 'rb')
+    loaded_obj = cPickle.load(apicklefile)
+    apicklefile.close()
+    return loaded_obj
+
+def pickle_gtypes(lineage_gtypes, indiv_gtypes, fname='pickle'):
+    # try to pickle the gtypes!
+    pklfile = ".".join([fname, 'aGtypes.pkl'])
+    apicklefile = open(pklfile, 'wb')
+    cPickle.dump(lineage_gtypes, apicklefile, -1)
+    apicklefile.close()
+
+    pklfile = ".".join([fname, 'iGtypes.pkl'])
+    ipicklefile = open(pklfile, 'wb')
+    cPickle.dump(indiv_gtypes, ipicklefile, -1)
+    ipicklefile.close()
+
+def unpickle_gtypes(fname='pickle'):
+    # and now to unpickle!
+    pklfile = ".".join([fname, 'aGtypes.pkl'])
+    apicklefile = open(pklfile, 'rb')
+    lineage_gtypes = cPickle.load(apicklefile)
+    apicklefile.close()
+
+    pklfile = ".".join([fname, 'iGtypes.pkl'])
+    ipicklefile = open(pklfile, 'rb')
+    indiv_gtypes = cPickle.load(ipicklefile)
+    ipicklefile.close()
+
+    return lineage_gtypes, indiv_gtypes
+
+def file_block(filehandle,  block, number_of_blocks=1000):
+    """
+    This code adapted from:
+    http://xor0110.wordpress.com/2013/04/13/how-to-read-a-chunk-of-lines-from-a-file-in-python/
+
+    Written by Nic Werneck
+
+    A generator that splits a file into blocks and iterates
+    over the lines of one of the blocks.
+
+    usage:
+    filehandle = open(filename)
+    number_of_chunks = 100
+    for chunk_number in range(number_of_chunks):
+        for line in file_block(filehandle, number_of_chunks, chunk_number):
+            process(line)
+    """
+
+
+    assert 0 <= block and block < number_of_blocks
+    assert 0 < number_of_blocks
+
+    filehandle.seek(0,2)
+    file_size = filehandle.tell()
+
+    ini = file_size * block / number_of_blocks
+    end = file_size * (1 + block) / number_of_blocks
+
+    if ini <= 0:
+        filehandle.seek(0)
+    else:
+        filehandle.seek(ini-1)
+        filehandle.readline()
+
+    while filehandle.tell() < end:
+        yield filehandle.readline()
+
+def create_log(args, outdir=None, outname='results'):
+    ## create output folder and log file of arguments:
+    timestamp = time.strftime("%b%d_%H.%M")
+    if outdir:
+        newfolder = os.path.realpath(outdir)
+        if os.path.exists(newfolder) is False:  # check to see if folder already exists...
+            os.mkdir(newfolder)
+        filename = newfolder + '/' + outname + '.' + timestamp + ".log"
+    else:
+        root_dir = os.getcwd()
+        newfolder = root_dir + "/" + outname + "." + timestamp
+        os.mkdir(newfolder)  # don't have to check if it exists, as timestamp is unique
+        filename = newfolder + "/" + outname + "." + timestamp + ".log"
+
+    log_h = open(filename, 'w')
+    log_h.write( "File created on %s\n" % (timestamp) )
+    log_h.write( "Program called from %s\n" % (os.getcwd()) )
+    log_h.write( "%s\n\n" % (' '.join(sys.argv)) )
+    for arg in str(args)[10:-1].split():
+        log_h.write( "%s\n" % (arg) )
+    log_h.close()
+    return filename
+
+def make_a_list(geneobj, col_num=0, readfile=True):
+    """given a path, list, dictionary or string, convert into a list of genes.
+    col_num specifies the column from which to extract the gene list from."""
+
+    # genefile can be given as a list of genes (say, from find_degs()... ), or as
+    # a path to a file containing a list of genes.
+    # The following builds a dictionary of genes from either input:
+    if type(geneobj) is list:   # allows filtering from hicluster generated list of results.
+        genelist = {}.fromkeys(geneobj,1)
+    elif type(geneobj) is dict:
+        genelist = geneobj # this of course will only work if the keys are the genes!
+    elif type(geneobj) is str:   # assuming a filepath...
+        if re.search("/",geneobj) is None:
+            genelist = {}.fromkeys(geneobj.split(','),1)
+        else:   # is a file path
+            if re.search(",",geneobj) or not readfile: # can turn of so that a single file path is returned as the string
+                genelist = {}.fromkeys(geneobj.split(','),1)
+            else:
+                genefile_h = open(geneobj, 'rb')
+                genelist = {}   # will be a dict of names { 'Cbir01255':1, 'CbirVgq':1, ... }
+                                # used a dictionary to automatically remove any name duplications
+                filegen = [ line.split() for line in genefile_h if len(line) > 0]
+
+                genefile_h.close()
+
+                for colset in filegen:
+                    try:
+                        genelist[colset[col_num]]=1
+                    except IndexError:
+                        verbalise("R", "Column %d not found in %s" % (col_num, str(colset)))
+    else:
+        genelist = {}
+
+    return genelist
+
 
 if __name__ == "__main__":
     rebuild_config()
