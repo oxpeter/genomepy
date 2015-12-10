cc7dc294665af665a61b8074d928d3072d68f173
oxpeter <oxpeter+git@gmail.com>
2015-12-09 20:42:41 -0500

    Improved primer specificity checking
    This was achieved in two ways: by switching away from BLAST to bwa,
    and by analysing each primer binding site to identify any
    potential PCR products (automating what previously had to be done
    by hand). These steps involved the creation of a whole suite of
    new functions:
    
    NEW check_pcr()
    
    This replaces an older version (now maintained as check_pcr_blast()
    It uses bwa to align both primers to the genome. The only filtering at
    this step is done at the level of the bwa parameters. Currently no
    ability to set bwa parameters, but will include in a future patch.
    
    NEW get_matches()
    
    Extracts all aligned matches from a SAM file and returns them as
    a dictionary keyed by scaffold with values as a list of hits.
    
    NEW find_pairs()
    
    takes two sets of aligned matches, and identifies any potential PCR
    products that could arise from them. CUrrently can set the maximum
    distance for considering a PCR product valid. Will add other filters
    such as number of mismatches or primer orientation in a future
    version.
    
    NEW pretty_pairs()
    
    A useful way to visualize the pcr_product pairs that are discovered.
    It displays the primer orientation, alignment lenth, number of
    mismatches, and length of putative product.
    
    NEW display_pcr_products()
    Displays the information from find pairs as pretty_pairs sorted by
    mismatch number and colorized.
    
    All primer pair analysis results are also being collated into a
    single dictionary keyed by gene name and primer sequence, to allow
    more appropriate reporting after all analyses are completed (not
    yet implemented).
    
    The main pipeline was also substantially altered in this update to
    utilize the new functions.

--- genomepy/qpcr.py
+++ genomepy/qpcr.py
@@ -6,10 +6,11 @@ import sys
 import argparse
 import re
 import datetime
+import tempfile
+import itertools
 
 from Bio.Seq import Seq
 from Bio.Alphabet import generic_dna, IUPAC
-import progressbar
 
 from genomepy import genematch, gffparser, cris, config
 
@@ -37,15 +38,66 @@ def define_arguments():
                         help="genome fasta file")
     parser.add_argument("-c", "--cds_file", type=str, default=dbpaths['cds'],
                         help="Fasta file of coding sequences")
-    parser.add_argument("-P", "--PCR", type=str,
+    parser.add_argument("-m", "--max_distance", type=int, default=500,
+                        help="""when checking primer mismatches, only report primer
+                        pairs that are less than max_distance from one another
+                        [ default = 500 ]""")
+    parser.add_argument("-p", "--pcr", type=str,
                         help="""file containing primer pairs for analysis (not yet
                         implemented""")
 
+
+
     return parser
 
-def check_PCR(primer1, primer2, gffobj, pcr_name="C_biroi", outputfile="~/tempfile", id_thresh=16, gap_thresh=1):
+def check_pcr(primer1, primer2, gffobj, pcr_name="C_biroi", outputfile="~/tempfile", id_thresh=16, gap_thresh=1):
+    """
+    uses bwa to align both primers to the genome. The only filtering at this step is
+    done at the level of the bwa parameters.
+    """
+
+    # set file paths:
+    temp_dir = tempfile.mkdtemp()
+    temp_primer1 = os.path.join(temp_dir, "primer1.fq")
+    temp_primer2 = os.path.join(temp_dir, "primer2.fq")
+    temp_sai1 = os.path.join(temp_dir, "primer1.sai")
+    temp_sai2 = os.path.join(temp_dir, "primer2.sai")
+    bwa_idx = '/Volumes/antqueen/genomics/indices/bwa/Cbir.assembly.v3.0.fa'
+
+    # create temp fq file for primer1 and primer2:
+    handle1 = open(temp_primer1, 'w')
+    handle1.write("@primer1\n%s\n+\n%s\n" % (primer1, '~' * len(primer1)))
+    handle1.close()
+
+    handle2 = open(temp_primer2, 'w')
+    handle2.write("@primer1\n%s\n+\n%s\n" % (primer2, '~' * len(primer2)))
+    handle2.close()
+
+    # set bwa alignment parameters:
+    bwa_aln   = "bwa aln -n 5 -k 4 -N -t 30 %s %s > %s"
+    bwa_samse = "bwa samse -n 10000000000  %s %s %s  > %s"
+
+    status_return1 = os.system(bwa_aln % (bwa_idx, temp_primer1,temp_sai1))
+    status_return2 = os.system(bwa_samse % (bwa_idx,
+                                            temp_sai1,
+                                            temp_primer1,
+                                            temp_sai1[:-3]+'sam'))
+    status_return3 = os.system(bwa_aln % (bwa_idx, temp_primer2,temp_sai2))
+    status_return4 = os.system(bwa_samse % (bwa_idx,
+                                            temp_sai2,
+                                            temp_primer2,
+                                            temp_sai2[:-3]+'sam'))
+
+    if status_return1 > 0 or status_return2 > 0 or status_return3 > 0 or status_return4 > 0:
+        verbalise("R", "Error performing bwa alignment for %s and %s" % (temp_primer1,
+                                                                            temp_primer2))
+
+    return (temp_sai1[:-3]+'sam', temp_sai2[:-3]+'sam')
+
+def check_pcr_blast(primer1, primer2, gffobj, pcr_name="C_biroi", outputfile="~/tempfile", id_thresh=16, gap_thresh=1):
     """creates pseudo PCR product from primers, and blasts C.biroi to look for multiple
-    PCR products."""
+    PCR products. This function is now deprecated in favor of the much faster check_pcr,
+    which uses bwa."""
     primer1_seq = Seq(primer1, generic_dna)
     primer2_seq = Seq(primer2, generic_dna).reverse_complement()
     PCR_product = primer1_seq + Seq("NNNNNNNNNNNNNNNNNNNNNNNNNNNNNN", generic_dna) + primer2_seq
@@ -63,6 +115,97 @@ def check_PCR(primer1, primer2, gffobj, pcr_name="C_biroi", outputfile="~/tempfi
                         shownearest=True
                         )
     cris.report_results(outfile + 'final_results.info', blast_dict)
+
+def get_matches(sam):
+    """
+    extracts all aligned matches from SAM file
+    returns dictionary:
+
+    { scaffold:[
+            (position, orientation, cigar, mismatches), ...
+                ]
+    }
+
+    """
+    matches_dic = {}
+    handle = open(sam, 'rb')
+
+    for line in handle:
+        if line[0] == '@':
+            continue
+        elif len(line.split()) < 12:
+            continue
+        else:
+            # calculate values for best match:
+            elements = line.split()
+            scaf = elements[2]
+            cigar = elements[5]
+            if elements[1] == '16':
+                orientation = '-'
+            else:
+                orientation = '+'
+            position = int(elements[3])
+            mismatches = int(elements[4]) # not entirely true, as it represents map score.
+
+            # add to dictionary:
+            matches_dic[scaf] = [(position,orientation,cigar,mismatches),]
+
+            # evaluate all secondary alignments:
+            for tag in elements[11:]:
+                if tag[:2] == 'XA':
+                    step1 = [ aln.split(',') for aln in tag[5:].split(';')]
+                    matches = [ (t[0],t[1][0],int(t[1][1:]),t[2],int(t[3])) for t in step1[:-1] ]
+                    for scaf,orientation,position,cigar,mismatches in matches:
+                        if scaf in matches_dic:
+                            matches_dic[scaf].append((position,orientation,cigar,mismatches))
+                        else:
+                            matches_dic[scaf] = [(position,orientation,cigar,mismatches),]
+    handle.close()
+    return matches_dic
+
+def pretty_pairs(p1, p2):
+    "create a visualisation of the pcr product to evaluate its quality"
+    if p1[0] < p2[0]:
+        pleft = p1
+        pright = p2
+    else:
+        pleft = p2
+        pright = p1
+
+    # set direction of template:
+    if pleft[1] == '-':
+        cleft = "%s:%d>>" % (pleft[2],pleft[3])
+    else:
+        cleft = "<<%s:%d" % (pleft[2],pleft[3])
+
+    if pright[1] == '+':
+        cright = "%s:%d>>" % (pright[2],pright[3])
+    else:
+        cright = "<<%s:%d" % (pright[2],pright[3])
+
+    return "%s---%d---%s" % (cleft,pright[0]-pleft[0],cright)
+
+def find_pairs(match1, match2, dist=500):
+    """
+    looks at two position dictionaries to see if there are any pairs within dist of each
+    other. Also considers pairs arising from within each match dictionary separately.
+    """
+    # create a merged dictionary with all positions labeled by scaffold:
+    combined = match1
+    for k in match2.keys():
+        if k in combined:
+            combined[k] += match2[k]
+
+    pairs = []
+    for s in combined:
+        if len(combined[s]) < 2:
+            continue    # if only one match on a scaffold, then it cannot form a PCR product!
+        else:
+            for p1,p2 in itertools.combinations(combined[s], 2):
+                if 0 < abs(p1[0] - p2[0]) <= dist:
+                    pairs.append((s, min(p1[0], p2[0]), p1[3] + p2[3], pretty_pairs(p1, p2)))
+
+    return pairs
 
 def parse_p3(input_file):
     p3_h = open(input_file, 'rb')
@@ -93,6 +236,10 @@ def parse_fold(fold_output):
             geneproduct = defmatch.group(2)
             primer1     = defmatch.group(3)
             primer2     = defmatch.group(4)
+        else:
+            geneproduct = "None found"
+            primer1 = "Primer1"
+            primer2 = "Primer2"
 
         if deltaGmatch:
             if float(deltaGmatch.group(1)) > -8:
@@ -101,11 +248,26 @@ def parse_fold(fold_output):
     fold_h.close()
     out_h.close()
 
+def display_pcr_products(parsed_data):
+    """
+    for colorful display of potential pcr products!
+    """
+    for p in sorted(parsed_data, key=lambda x: x[2]):
+        if p[2] == 0:
+            verbalise("G", p)
+        elif 0 < p[2] <= 2:
+            verbalise("C", p)
+        elif 2 < p[2] <= 4:
+            verbalise("Y", p)
+        elif 4 < p[2] <= 6:
+            verbalise("M", p)
+        elif 6 < p[2]:
+            verbalise("R", p)
+
 
 def main(args, logfile):
     genelist = config.make_a_list(args.gene_list)
-    #print "\nCreating genome parsing objects..."
-    #gffobj = gffparser.assemble_dict()
+
     verbalise("B", "Assembling gff file...")
     quickinfo = gffparser.My_gff()
 
@@ -120,7 +282,6 @@ def main(args, logfile):
     cumcount = 0
     for geneid in genelist:
         ## get sequence:
-        #seq = genematch.extractseq(geneid)
         isoseqs = gfflib.extractseq(geneid, cds=True)
         boundaries = gfflib.extractseq(geneid, boundaries=True) # this is a dictionary
         exon_boundaries = {}
@@ -138,7 +299,7 @@ def main(args, logfile):
 
             for boundary in exon_boundaries[isoform][:-1]:
                 # format for primer3 input file:
-                input_txt = "SEQUENCE_ID=%s_exon%d\n" % (geneid, count) + \
+                input_txt = "SEQUENCE_ID=%s_exon%d\n" % (isoform, count) + \
                 "SEQUENCE_TEMPLATE=%s\n" % (isoseqs[isoform]) + \
                 "SEQUENCE_TARGET=%d,2\n" % (boundary) +\
                 "PRIMER_THERMODYNAMIC_PARAMETERS_PATH=/home/peter/lib/primer3_config/\n" + \
@@ -175,35 +336,58 @@ def main(args, logfile):
     cmd = "primer3_core -format_output -output=%s %s" % (p3_out, p3_input)
     os.system(cmd)
 
-    ## blast primer3 primer pairs to check for specificity, and check for tertiary structure:
-    # parse primer3 output to extract primer pairs:
+
+
+
     print "\nAnalysing ~%d primer pairs:" % (cumcount * 5)
 
-    p3_gen = parse_p3(p3_out)
+    # parse the primer3 output file
+    p3_results = parse_p3(p3_out)
+
+    # set up a dictionary to collate all the analyses performed on the primer pairs
+    """
+    all_results[(genename, primer1, primer2)] = {  'primers':(),
+                                                   'pcr_products':(),
+                                                   'rna_fold':(),
+
+                                                }
+    """
+    all_results = {}
 
 
-    #bar = progressbar.ProgressBar(maxval=cumcount*5, \
-    #widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.ETA()])
     count=0
     t0 = datetime.datetime.now()
-    #bar.update(count)
-    for name, primer1, primer2, start, finish in p3_gen:
+
+    for name, primer1, primer2, start, finish in p3_results:
         count += 1
-        #print "Processing primer pair for %s" % (name)
-        # blast primer pair:
-        blastout = logfile[:-3] + ".".join([name, primer1, 'blastn'])
-        check_PCR(primer1, primer2, pcr_name=name, outputfile=blastout, gffobj=gfflib)
+        all_results[(name, primer1, primer2)] = {'primers':(name,primer1,primer2,start,finish)}
 
-        ## check blastn output for viable PCR products:
-        #hitnumber, hitseq = cris.showblast(filename=blastout + ".blastout.tmp", \
-        #    threshold=80, gaps_allowed=3, shownearest=False, \
-        #    seq="_unknown_", out_path=blastout + ".parsed.info")
+        # align primers to genome:
+        blastout = logfile[:-3] + ".".join([name.split(' ')[0][4:10], primer1, 'blastn'])
+        sam1, sam2 = check_pcr(primer1, primer2, pcr_name=name, outputfile=blastout, gffobj=gfflib)
 
-        # prepare to check for tertiary structure:
-        # extract PCR product and save to fasta file:
-        geneid = re.search("(.*)_exon", name).group(1)
-        pcr_seq_dic = gfflib.extractseq(geneid, cds=True, trim_from=start , trim_to=finish )
-        pcr_seq = pcr_seq_dic[isoform]
+        # check for non-specific primer pairs:
+        match1 = get_matches(sam1)
+        match2 = get_matches(sam2)
+        pairs = find_pairs(match1, match2, dist=args.max_distance)
+
+        # clean up temporary files
+        temp_dir = os.path.dirname(sam1)
+        for f in os.listdir(temp_dir):
+            os.remove(os.path.join(temp_dir,f))
+        os.rmdir(temp_dir)
+
+        # save results  - display using display_pcr_products()
+        all_results[(name, primer1, primer2)]['pcr_products'] = sorted(pairs, key=lambda x: x[2])
+
+        # extract PCR product and save to fasta file for tertiary structure analysis:
+        isoform = re.search("(.*)_exon", name).group(1)
+        pcr_seq_dic = gfflib.extractseq(isoform.split(" ")[2][5:], cds=True, trim_from=start , trim_to=finish )
+        try:
+            pcr_seq = pcr_seq_dic[isoform]
+        except KeyError:
+            verbalise("R", isoform, isoform.split(" ")[2][5:], "\n\n", pcr_seq_dic)
+            exit()
         fastafile = logfile[:-3] + 'fa'
         defline = " ".join([name, primer1, primer2])
         cris.make_fasta(pcr_seq, seqnames=defline, outpath=fastafile, append='a')
@@ -213,20 +397,18 @@ def main(args, logfile):
         tdiff = (t1-t0)
         trate = tdiff / count
         tleft = trate * cumcount * 5 - tdiff
-
-        sys.stdout.write("\rAnalysed %d/%d (%.2f%%) pairs. Approx %s remaining         \r" % (count,
+        sys.stdout.write("\nAnalysed %d/%d (%.2f%%) pairs. Approx %s remaining\n\n" % (count,
                                                         cumcount * 5,
                                                         100.0 * count / (cumcount * 5),
                                                         tleft ))
         sys.stdout.flush()
-        #bar.update(count)
-    #bar.finish()
 
     ## run VIENNA RNAfold to check tertiary structure
     print "Running RNAfold..."
-    viennaout = args.output + ".RNAfold.info"
+    viennaout = logfile[:-3] + "RNAfold.info"
     cmd = "RNAfold -p -d2 --noLP -T 60 < " + fastafile + " > " + viennaout
-    os.system(cmd)
+    if os.system(cmd) > 0:
+        verbalise("R", "Error running RNAfold")
 
     ## cull VIENNA output to acceptable structure list, output primers:
     parse_fold(viennaout)
@@ -244,7 +426,7 @@ if __name__ == "__main__":
     verbalise = config.check_verbose(not(args.quiet))
     logfile = config.create_log(args, outdir=args.directory, outname=args.output)
 
-    if args.PCR:
+    if args.pcr:
         pass
     else:
         main(args, logfile)
